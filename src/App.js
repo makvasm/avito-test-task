ae81508f9ac29f87837fa493868abb284893b136
makvasm <makvasm111@gmail.com>
2020-08-31 17:38:02 +0300

    css fixes

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
 
     return (
         <Router history={history}>
-            <ThemeSwitcher width={75} height={25} />
+            <ThemeSwitcher width={50} height={25} />
 
             <Suspense fallback={<Spinner animation="border" />}>
                 <Gallery images={images} />
